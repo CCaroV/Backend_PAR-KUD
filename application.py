8df9a8d5565dd967f111a0e8ca0ce3f9610997e3
Andrés Beltrán <andres.beltran@citi.com.mx>
2023-05-28 21:32:59 -0500

    Prueba Azure

--- application.py
+++ application.py
@@ -1,6 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from routes.parking import routes_parking
+from routes.token import routes_token
+from routes.users import *
 
-@app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+app = Flask(__name__)
+cors = CORS(app)
+
+app.register_blueprint(routes_user)
+app.register_blueprint(routes_user_auth)
+app.register_blueprint(routes_SUser)
+app.register_blueprint(routes_token)
+app.register_blueprint(routes_parking)
+
+
+@app.route("/", methods=['GET'])
+def test():
+    json = {}
+    json["message"] = "Server running ..."
+    return jsonify(json)
+
+
+if __name__ == '__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : " + "http://" + dataConfig["host"] + ":" + str(dataConfig["port"]))
+    app.run()
