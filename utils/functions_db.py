5048f0ef3f9266fce489509432a5d41528bf00f7
Andrés Beltrán <andres.beltran@citi.com.mx>
2023-05-31 20:25:19 -0500

    se reanuda consumo desde .env

--- utils/functions_db.py
+++ utils/functions_db.py
@@ -1,11 +1,8 @@
-import base64
-from os import getenv
-
 import psycopg2
 from flask import Flask, json, Request
 
-from utils.encrypter import decrypt_dict
 from utils.function_jwt import get_data
+from utils.encrypter import decrypt_dict
 
 app = Flask(__name__)
 
@@ -18,8 +15,7 @@ def loadFileConfig():
 
 def conectarBD(request: Request):
     token = request.headers['Authorization'].split(" ")[1]
-    key = base64.urlsafe_b64encode("0bImZx-AaHdk7E3Aoa6-FS5BKzkIrTc=".encode("utf-8"))
-    data = decrypt_dict(get_data(token),key)
+    data = decrypt_dict(get_data(token))
     dataConn = loadFileConfig()
     conn = psycopg2.connect(host=dataConn["host"], port=dataConn["port"], database=dataConn["database"],
                             user=data["user"], password=data["password"])
