fea82a3de5c74d0558cc2addd3b090b8e17bdbe4
Andabero5 <adbeltranr@correo.udistrital.edu.co>
2023-06-03 19:46:02 -0500

    se añade patron singleton y cambio de permisos para registro

--- utils/functions_db.py
+++ utils/functions_db.py
@@ -10,17 +10,29 @@ from utils.function_jwt import get_data
 app = Flask(__name__)
 
 
-def loadFileConfig():
-    with open('./config.json') as f:
-        data = json.load(f)
-    return data
+# patron singleton para los datos de la base de datos desde el json
+class ConfigLoader:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._config = None
+        return cls._instance
+
+    def loadFileConfig(self):
+        if self._config is None:
+            with open('./config.json') as f:
+                self._config = json.load(f)
+        return self._config
 
 
 def conectarBD(request: Request):
     token = request.headers['Authorization'].split(" ")[1]
     key = "NwKyZF848xFHInNmjQfY4U4uKTNGhK2ABqgHvolGhuA="
     data = decrypt_dict(get_data(token), key)
-    dataConn = loadFileConfig()
+    config_loader = ConfigLoader()
+    dataConn = config_loader.loadFileConfig()
     conn = psycopg2.connect(host=dataConn["host"], port=dataConn["port"], database=dataConn["database"],
                             user=data["user"], password=data["password"])
     return conn
@@ -28,14 +40,16 @@ def conectarBD(request: Request):
 
 def conectarBDAuth(request: Request):
     key = "NwKyZF848xFHInNmjQfY4U4uKTNGhK2ABqgHvolGhuA="
-    dataConn = loadFileConfig()
+    config_loader = ConfigLoader()
+    dataConn = config_loader.loadFileConfig()
     conn = psycopg2.connect(host=dataConn["host"], port=dataConn["port"], database=dataConn["database"],
-                            user="parkud_db_admin", password="20181020027")
+                            user="manage_account_user", password="CA1234")
     return conn
 
 
 def conectarBDLog(request: Request):
-    dataConn = loadFileConfig()
+    config_loader = ConfigLoader()
+    dataConn = config_loader.loadFileConfig()
     conn = psycopg2.connect(host=dataConn["host"], port=dataConn["port"], database=dataConn["database"],
                             user=request['user'], password=request["password"])
     return conn
